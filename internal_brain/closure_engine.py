c87b53b6cbe6beb48da913a3b8c904c16d42284b
StegVerse <rigelrandolph@gmail.com>
2026-03-24 17:38:24 -0500

    Create closure_engine.py

---
+++ internal_brain/closure_engine.py
@@ -0,0 +1,7 @@
+def compute_closure(change, state):
+    return {
+        "affected_files": [...],
+        "dependent_bundles": [...],
+        "tests_to_update": [...],
+        "receipts_to_regenerate": [...]
+    }
